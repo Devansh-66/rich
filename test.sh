209d01d005f65e5bedab1995035c8a04c8809eed
Devansh Rajput <devanshrajput960@gmail.com>
2026-05-01 20:16:59 +0530

    Add test.sh script for running pytest with options

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+OUTPUT=""
+MODE=""
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --output_path)
+      OUTPUT="$2"
+      shift 2
+      ;;
+    base|new)
+      MODE="$1"
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+if [ -z "$OUTPUT" ]; then
+  OUTPUT="results.xml"
+fi
+
+pytest tests/test_panel_columns_expand.py --junitxml="$OUTPUT"
